eeb07a258ca50d65724daeb2829bbb62c4156b73
Nanda Pratama Putra <60767596+nandappputra@users.noreply.github.com>
2023-02-04 17:23:48 +0700

    feat: add method to generate dog from data URL

--- src/models/Dog.mjs
+++ src/models/Dog.mjs
@@ -5,12 +5,133 @@ import * as metaPNG from "meta-png";
 
 import partProperties from "../assets/partConfiguration";
 import { PARTS } from "../utils/constants.mjs";
+import {
+  convertMetadataStringToUint8Array,
+  convertPNGDataURLToUint8Array,
+  getMetadataFromUint8Array,
+} from "../utils/ImageUtils.mjs";
 
 class Dog {
-  constructor(gene, signedHash = null, publicKey = null) {
+  constructor(
+    gene,
+    signedHash = null,
+    publicKey = null,
+    parent1Gene = null,
+    parent2Gene = null,
+    parent2PublicKey = null,
+    parent1SignedHash = null,
+    parent2SignedHash = null,
+    parentMarriageHash = null
+  ) {
     this.gene = gene;
     this.signedHash = signedHash;
     this.publicKey = publicKey;
+    this.parent1Gene = parent1Gene;
+    this.parent2Gene = parent2Gene;
+    this.parent2PublicKey = parent2PublicKey;
+    this.parent1SignedHash = parent1SignedHash;
+    this.parent2SignedHash = parent2SignedHash;
+    this.parentMarriageHash = parentMarriageHash;
+  }
+
+  static buildDogFromDataURL(dataURL) {
+    const pngArray = convertPNGDataURLToUint8Array(dataURL);
+    let gene = null,
+      signedHash = null,
+      publicKey = null,
+      parent1Gene = null,
+      parent2Gene = null,
+      parent2PublicKey = null,
+      parent1SignedHash = null,
+      parent2SignedHash = null,
+      parentMarriageHash = null;
+
+    const geneMeta = getMetadataFromUint8Array(pngArray, "pawgenics_gene");
+    if (geneMeta) {
+      gene = JSON.parse(geneMeta);
+    }
+
+    const signedHashMeta = getMetadataFromUint8Array(
+      pngArray,
+      "pawgenics_signedHash"
+    );
+    if (signedHashMeta) {
+      signedHash = convertMetadataStringToUint8Array(signedHashMeta);
+    }
+
+    const publicKeyMeta = getMetadataFromUint8Array(
+      pngArray,
+      "pawgenics_publicKey"
+    );
+    if (publicKeyMeta) {
+      publicKey = convertMetadataStringToUint8Array(publicKeyMeta);
+    }
+
+    const parent1GeneMeta = getMetadataFromUint8Array(
+      pngArray,
+      "pawgenics_parent1Gene"
+    );
+    if (parent1GeneMeta) {
+      parent1Gene = JSON.parse(parent1GeneMeta);
+    }
+
+    const parent2GeneMeta = getMetadataFromUint8Array(
+      pngArray,
+      "pawgenics_parent2Gene"
+    );
+    if (parent2GeneMeta) {
+      parent2Gene = JSON.parse(parent2GeneMeta);
+    }
+
+    const parent2PulicKeyMeta = getMetadataFromUint8Array(
+      pngArray,
+      "pawgenics_parent2PublicKey"
+    );
+    if (parent2PulicKeyMeta) {
+      parent2PublicKey = convertMetadataStringToUint8Array(parent2PulicKeyMeta);
+    }
+
+    const parent1SignedHashMeta = getMetadataFromUint8Array(
+      pngArray,
+      "pawgenics_parent1SignedHash"
+    );
+    if (parent1SignedHashMeta) {
+      parent1SignedHash = convertMetadataStringToUint8Array(
+        parent1SignedHashMeta
+      );
+    }
+
+    const parent2SignedHashMeta = getMetadataFromUint8Array(
+      pngArray,
+      "pawgenics_parent2SignedHash"
+    );
+    if (parent2SignedHashMeta) {
+      parent2SignedHash = convertMetadataStringToUint8Array(
+        parent2SignedHashMeta
+      );
+    }
+
+    const parentMarriageHashMeta = getMetadataFromUint8Array(
+      pngArray,
+      "pawgenics_parentMarriageHash"
+    );
+    if (parentMarriageHashMeta) {
+      parentMarriageHash = convertMetadataStringToUint8Array(
+        parentMarriageHashMeta
+      );
+    }
+
+    return new Dog(
+      gene,
+      signedHash,
+      publicKey,
+      parent1Gene,
+      parent2Gene,
+      parent2PublicKey,
+      parent1SignedHash,
+      parent2SignedHash,
+      parentMarriageHash
+    );
   }
 
   static async buildDog(name, uuid) {
