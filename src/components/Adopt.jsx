57efd9dae6a0b73e0cec0dd8c5d6d7fa456828fb
Nanda Pratama Putra <60767596+nandappputra@users.noreply.github.com>
2023-02-12 18:29:29 +0700

    feat: modify UI for Adopt and Home page

--- src/components/Adopt.jsx
+++ src/components/Adopt.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import DogPicture from "./DogPicture.jsx";
 import GeneService from "../services/GeneService.mjs";
+import { Button, Container, Row, Col, Card } from "react-bootstrap";
 
 const Adopt = () => {
   const [adoptedDog, setAdoptedDog] = useState(null);
@@ -13,12 +14,28 @@ const Adopt = () => {
   };
 
   return (
-    <div>
-      <h1>Adopt</h1>
-      <button onClick={generateDog}>Adopt a dog!</button>
+    <Container>
+      <h1 style={{ padding: "0.5em 0 0.5em 0" }} className="text-center">
+        Adopt
+      </h1>
+      <p className="text-center">Adopt a new dog!</p>
+      <Row className="justify-content-md-center text-center">
+        <Col md="auto">
+          <Card style={{ width: "18rem", cursor: "pointer" }}>
+            <Card.Body className="justify-content-md-center">
+              <Card.Title className="text-center">Look Around</Card.Title>
+              <Card.Subtitle className="mb-2 text-muted">
+                There might be a good dog waiting for you!
+              </Card.Subtitle>
+              <Button onClick={generateDog}>Adopt a dog!</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+
       {adoptedDog && <DogPicture dog={adoptedDog} id="dogNew" />}
       {privateKey && <img id="key" src={privateKey} />}
-    </div>
+    </Container>
   );
 };
 
