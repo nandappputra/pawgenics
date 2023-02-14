94ca9df990c0b8b03f648fa57ab3a934f1d3d700
Nanda Pratama Putra <60767596+nandappputra@users.noreply.github.com>
2023-02-14 22:17:44 +0700

    feat: add icon to the home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,12 +1,31 @@
-import { Container } from "react-bootstrap";
+import { Col, Row, Container } from "react-bootstrap";
 
 const Home = () => {
   return (
     <Container>
-      <h1 style={{ padding: "0.5em 0 0.5em 0" }} className="text-center">
-        Pawgenics
-      </h1>
-      <p className="text-center">Adopt & breed adorable digital puppies!</p>
+      <Row className="justify-content-md-center">
+        <Col md="auto">
+          <img
+            src="/logo.svg"
+            width="100"
+            height="100"
+            className="text-center"
+            style={{ margin: "1em" }}
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <h1 style={{ padding: "0.5em 0 0.5em 0" }} className="text-center">
+            Pawgenics
+          </h1>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <p className="text-center">Adopt & breed adorable digital puppies!</p>
+        </Col>
+      </Row>
     </Container>
   );
 };
